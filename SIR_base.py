76cd19066b28e3a8b64869de3c4bad0b55a68c06
Mateus de Jesus Mendes <mateus.jesusmd@gmail.com>
2025-06-21 13:05:36 -0300

    Código base do projeto

--- SIR_base.py
+++ SIR_base.py
@@ -1,24 +1,36 @@
 import numpy as np
 from scipy.integrate import odeint
 
-def executar_sir(): 
-        N = int(input('População Total: '))
-        I0 = int(input('Infectados iniciais: '))
-        R0 = 0
-        S0 = N - I0 - R0
-        beta = float(input('Taxa de Transmissão (β): '))
-        gamma = float(input('Taxa de Recuperação (γ): '))
-        dias = int(input('Dias de simulação: '))
 
-        t = np.linspace(0, dias, dias)
+def modelo_sir(vetor, t):
+    """
+    Calcula as derivadas das variáveis do modelo epidemiológico SIR
 
-        def modelo_sir(vetor, t):
-            S, I, R = vetor
-            dS = -beta * S * I / N
-            dI = beta * S * I / N - gamma * I
-            dR = gamma * I
-            return [dS, dI, dR]
+    Parâmetros:
+    vetor: lista ou array contendo os valores atuais de [S, I, R] no tempo t
+    t: tempo atual (passado automaticamente por odeint e não usado diretamente nessa função)
 
-        resultado = odeint(modelo_sir, [S0, I0, R0], t)
+    Retorna:
+    Retorna as variações populacionais (dS/dt, dI/dt, dR/dt) para cada compartimento no tempo t
+    """
+    S, I, R = vetor
+    dS = -beta * S * I / N
+    dI = beta * S * I / N - gamma * I
+    dR = gamma * I
+    return [dS, dI, dR]
 
-        return resultado
+
+# Definição e obtenção dos parâmetros
+N = int(input('População Total: '))
+I0 = int(input('Infectados iniciais: '))
+R0 = 0 # Recuperados iniciais
+S0 = N - I0 - R0 # Susceptíveis iniciais
+beta = float(input('Taxa de Transmissão (β): '))
+gamma = float(input('Taxa de Recuperação (γ): '))
+dias = int(input('Dias de simulação: '))
+
+# Período da simulação (dias)
+t = np.linspace(0, dias, dias)
+
+# Integra numericamente o sistema de equações diferenciais ao longo do período definido (t)
+resultado = odeint(modelo_sir, [S0, I0, R0], t)
