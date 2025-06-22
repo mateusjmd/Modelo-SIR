fb3f93bc8633dfe32816d876ad9bbd71390e3046
Mateus de Jesus Mendes <mateus.jesusmd@gmail.com>
2025-06-22 14:10:28 -0300

    Script central

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ import streamlit as st
 from SIR import executar_sir
 from SIRD import executar_sird
 from SIRD_duplo import executar_sird_duplo
+from SIRD_vital import executar_sird_vital
+
 
 # Título geral
 st.set_page_config(page_title='Modelos Epidemiológicos')
 st.title('Simulador de Modelos Epidemiológicos')
 
-centered_title = '''
+# Configurações em CSS para estilização da página
+centered_title = """
 <style>
 h1 {
     text-align: center;
 }
 </style>
-'''
+"""
 
 page_bg_img = """
 <style>
@@ -28,48 +31,50 @@ h1 {
 </style>
 """
 
-# Conteúdo do app
+# Ativa as alterações em CSS do dashboard
+st.markdown(centered_title, unsafe_allow_html=True)
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Interface inicial com dropdown
 modelos = ['Selecione um modelo',
            'SIR',
            'SIRD',
-           'SIRD - Dupla População Interagente',
+           'SIRD - Dupla População Interagente (Simplificado)',
            'SIRD - Dinâmica Vital']
 modelo_selecionado = st.selectbox('Escolha um modelo:', modelos)
 
 # Oculta a interface até que um modelo seja selecionado
 match modelo_selecionado:
     case 'Selecione um modelo':
-
-        # Divisão em duas colunas
+        # Estrutura da interface inicial em duas colunas
         col1, col2 = st.columns(2)
 
-        # Conteúdo da Coluna 1
         with col1:
-            st.subheader('Autores')
-            st.write('Giovani M. Nagano')
-            st.write('Letícia Nunes')
-            st.write('Lília Gavazza')
-            st.write('Mateus J. Mendes')
+            st.subheader('Desenvolvedores')
+            st.markdown('[Giovani Massayuki Miranda Nagano](https://github.com/giovaninagano5)')
+            st.markdown('[Letícia Nunes de Souza Andrade](https://github.com/LeticiaNunesAndrade)')
+            st.markdown('[Lília Helena Gavazza Pessoa](https://github.com/LiliaGavazza)')
+            st.markdown('[Mateus de Jesus Mendes](https://github.com/mateusjmd)')
 
-        # Conteúdo da Coluna 2
         with col2:
             st.subheader('Orientadores')
-            st.write('Daniel Roberto Cassar')
-            st.write('Leandro Lemos')
-            st.write('James Almeida')
-            st.write('Vinicius Francisco Wasquez')
-        
+            st.markdown('[Daniel Roberto Cassar](https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4262774J5)')
+            st.markdown('[James Moraes de Almeida](https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4710448J1)')
+            st.markdown('[Leandro Nascimento Lemos](https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4278041J7)')
+            st.markdown('[Vinícius Francisco Wasques](https://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4355089T5)')
+    
     case 'SIR':
+        # Executa o modelo SIR
         executar_sir()
 
     case 'SIRD':
+        # Executa o modelo SIRD
         executar_sird()
 
-    case 'SIRD - Dupla População Interagente':
+    case 'SIRD - Dupla População Interagente (Simplificado)':
+        # Executa o modelo SIRD de dupla população interagente
         executar_sird_duplo()
 
     case 'SIRD - Dinâmica Vital':
-        executar_sird_duplo()
+        # Executa o modelo SIRD de dinâmica vital
+        executar_sird_vital()
